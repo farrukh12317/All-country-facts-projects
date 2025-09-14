6e5a889589064a9d05626120466c73bc89424c56
Farrukh shah <sfarrukh033@gmail.com>
2025-09-15 02:03:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { Country } from "./pages/Country";
 import AppLayout from './components/Layout/AppLayout'
 import ErrorPage from './components/ui/ErrorPage'
 // import "./App.css";
-import '../Apptest.css'
+import './Apptest.css'
 
 let router = createBrowserRouter([
   {
